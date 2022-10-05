688b8cf736b3036654d150738b49c91384142486
maryauc <93332789+maryauc@users.noreply.github.com>
2022-10-05 14:55:49 +0700

    Update MainActivity.java

--- app/src/main/java/id/ac/polinema/intentexercise/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/MainActivity.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Button btnRegistrasi;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
-	public void handleRegister(View view) {
-		Intent intent = new Intent(this, RegisterActivity.class);
-		startActivity(intent);
+		btnRegistrasi = findViewById(R.id.btn_register);
+
+		btnRegistrasi.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent pindah = new Intent(MainActivity.this, RegisterActivity.class);
+				startActivity(pindah);
+			}
+		});
 	}
 }
